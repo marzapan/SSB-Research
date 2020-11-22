cc9b2c5c4b9545ae2c774f880288c015f8592212
marzapan <71137934+marzapan@users.noreply.github.com>
2020-11-22 15:31:42 -0500

    Add files via upload

--- Add_to_Blob.js
+++ Add_to_Blob.js
@@ -1,17 +1,18 @@
-const ssbClient = require('ssb-client')
-var fs = require('fs')
-var pull = require('pull-stream')
-var toPull = require('stream-to-pull-stream')
-ssbClient((err, sbot)=>{
+const ssbClient = require('ssb-client') //Imports ssb-client
+var fs = require('fs') //Imports fs
+var pull = require('pull-stream') //Imports pull
+var toPull = require('stream-to-pull-stream')//Imports toPull
+ssbClient((err, sbot)=>{ //Connects the the ssb-client network
     pull(
-        toPull.source(fs.createReadStream('./trio.jpeg')),
+        toPull.source(fs.createReadStream('./trio.jpeg')),  //creates a new-blob in .ssb and adds the file to it
         sbot.blobs.add(function (err, hash) {
-          console.log(hash)
+          console.log(hash) //prints the blob so that we can get the key to retrieve the file from the blob
         })
     )
  
 })
     
+
 
 
 
--- Get_blob.js
+++ Get_blob.js
@@ -1,13 +1,13 @@
-const ssbClient = require('ssb-client')
-const pull = require('pull-stream')
-ssbClient((err, sbot) => {
+const ssbClient = require('ssb-client') //Imports ssb-client
+const pull = require('pull-stream') //Imports pull-stream
+ssbClient((err, sbot) => { //Connects to the ssb-client network 
     pull(
-        sbot.blobs.get('&PzrWL135YoG6zfNMw+XjFwt1824QQ4Vs8jej3Ab9BFM=.sha256'),
+        sbot.blobs.get('&PzrWL135YoG6zfNMw+XjFwt1824QQ4Vs8jej3Ab9BFM=.sha256'), //Once hash of blob is known. Anyone with the hash can call the file stored in the blob
         pull.collect((err, values)=> {
             const data = values.join('')
-            console.log(data)
+            console.log(data) //prints data to the terminal
         })
     )
 
-    sbot.close()
+    sbot.close() //closes script
 })
